e5fd8efb9c34d6ba163be0a86ca57635ae173b07
SGDoubler <doublesn@mail.uc.edu>
2023-06-11 13:07:03 -0400

    Sales Tax Task 1 Done

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double purchasePrice = 25.0;
+        double salesTax = .05;
+
+        double computedSalesTax = purchasePrice * salesTax;
+
+        System.out.println("The Sales tax for " + purchasePrice + " is " + computedSalesTax);
+
+    }
+}
